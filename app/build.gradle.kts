db3c44ab56198731f256107e7740e033b43b033e
subedisulav850-a11y <subedisulav850@gmail.com>
2026-09-06 19:25:23 +0545

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sulav.proxy"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sulav.proxy"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
