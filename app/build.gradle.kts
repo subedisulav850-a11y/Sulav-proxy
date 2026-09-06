991b3f24499127b39b5738cc2d781e0159de33c4
subedisulav850-a11y <subedisulav850@gmail.com>
2026-09-06 22:10:56 +0545

    Modify androidx dependencies in build.gradle.kts
    Updated dependencies for androidx.activity and androidx.compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+       implementation("androidx.activity:activity-compose:1.9.3")
+   implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
